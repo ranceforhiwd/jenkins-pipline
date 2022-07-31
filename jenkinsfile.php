0d1f9a36ad651b0feb13dda7d8921b76eff38c45
Rance Aaron <ranceaaron941@gmail.com>
2022-07-31 17:24:59 -0400

    add jenkinsfile

---
+++ jenkinsfile.php
@@ -0,0 +1,11 @@
+Jenkinsfile (Declarative Pipeline)
+pipeline {
+    agent { docker { image 'php:8.1.0-alpine' } }
+    stages {
+        stage('build') {
+            steps {
+                sh 'php --version'
+            }
+        }
+    }
+}
