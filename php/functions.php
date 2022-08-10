e50348890e5c458b6e1f4ecd222ffd4ffc312721
Rance Aaron <ranceaaron941@gmail.com>
2022-08-10 06:39:13 -0400

    removed unused file

--- php/functions.php
+++ php/functions.php
@@ -4,7 +4,7 @@ function setGlobalVariable()
     $GLOBALS['endpoint_base'] = 'http://ofc.quickfixtrips.fun/api/index.php/';    
     $GLOBALS['date_today'] = date("Y-m-d");
     $GLOBALS['api_key'] = '9ZsSplB25p26JP8sYcvcRy6wY6w8HO8X';  
-    //$GLOBALS['api_key'] = ${APIKEY};  
+     
 }
 
 // function to call a REST API
